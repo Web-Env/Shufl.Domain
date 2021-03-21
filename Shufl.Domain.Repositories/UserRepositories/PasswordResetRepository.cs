403d2902542c043d8c77fdabc0cb753cc20f119c
AdamBOD <adambarryodonovan@gmail.com>
2021-03-21 16:37:35 +0000

    Setup FetchByIdentifier methods for UserVerificationRepository & PasswordResetRepository

--- Shufl.Domain.Repositories/UserRepositories/PasswordResetRepository.cs
+++ Shufl.Domain.Repositories/UserRepositories/PasswordResetRepository.cs
@@ -1,10 +1,21 @@
-﻿using Shufl.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.UserRepositories.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.UserRepositories
 {
     public class PasswordResetRepository : RepositoryBase<PasswordReset>, IPasswordResetRepository
     {
         public PasswordResetRepository(ShuflContext context) : base(context) { }
+
+        public async Task<PasswordReset> FetchByIdentifier(string resetIdentifier)
+        {
+            var passwordReset = await _ShuflContext.PasswordResets.Where(pr =>
+                pr.ResetIdentifier == resetIdentifier).FirstOrDefaultAsync();
+
+            return passwordReset;
+        }
     }
 }
