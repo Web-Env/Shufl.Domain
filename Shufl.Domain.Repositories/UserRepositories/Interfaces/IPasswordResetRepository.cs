8d7fc6d1b3de066a5e41be367d27044bd90e24d7
AdamBOD <adambarryodonovan@gmail.com>
2021-03-21 16:44:39 +0000

    Fixed Interfaces

--- Shufl.Domain.Repositories/UserRepositories/Interfaces/IPasswordResetRepository.cs
+++ Shufl.Domain.Repositories/UserRepositories/Interfaces/IPasswordResetRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Interfaces;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.UserRepositories.Interfaces
 {
     public interface IPasswordResetRepository : IRepositoryBase<PasswordReset>
     {
+        Task<PasswordReset> FetchByIdentifier(string resetIdentifier);
     }
 }
