28c00b6420d2760c3405ea57a4da2cdf8d9af298
AdamBOD <adambarryodonovan@gmail.com>
2021-03-21 16:46:28 +0000

    Fixed Interfaces (#7)
    * Setup FetchByIdentifier methods for UserVerificationRepository & PasswordResetRepository
    
    * Fixed Interfaces

--- Shufl.Domain.Repositories/UserRepositories/Interfaces/IUserVerificationRepository.cs
+++ Shufl.Domain.Repositories/UserRepositories/Interfaces/IUserVerificationRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Interfaces;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.UserRepositories.Interfaces
 {
     public interface IUserVerificationRepository : IRepositoryBase<UserVerification>
     {
+        Task<UserVerification> FetchByIdentifier(string verificationIdentifier);
     }
 }
