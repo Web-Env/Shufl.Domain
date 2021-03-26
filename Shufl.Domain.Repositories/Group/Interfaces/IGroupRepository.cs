dead557ba69db39932b1f3f18721d7eb260f4bf4
AdamBOD <adambarryodonovan@gmail.com>
2021-03-26 17:28:45 +0000

    Added GetByIdentifier method (#12)

--- Shufl.Domain.Repositories/Group/Interfaces/IGroupRepository.cs
+++ Shufl.Domain.Repositories/Group/Interfaces/IGroupRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Shufl.Domain.Repositories.Interfaces;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group.Interfaces
 {
     public interface IGroupRepository : IRepositoryBase<Entities.Group>
     {
+        Task<Entities.Group> GetByIdentifier(string groupIdentifier);
     }
 }
