3905bd821959a3eca0fbff0bdebb60a74694913d
AdamBOD <adambarryodonovan@gmail.com>
2021-03-27 19:09:25 +0000

    Added GetByUserIdAndGroupIdAsync to GroupMemberRepository

--- Shufl.Domain.Repositories/Group/GroupMemberRepository.cs
+++ Shufl.Domain.Repositories/Group/GroupMemberRepository.cs
@@ -1,10 +1,21 @@
-﻿using Shufl.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Group.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group
 {
     public class GroupMemberRepository : RepositoryBase<GroupMember>, IGroupMemberRepository
     {
         public GroupMemberRepository(ShuflContext context) : base (context) { }
+
+        public async Task<GroupMember> GetByUserIdAndGroupIdAsync(Guid userId, Guid groupId)
+        {
+            return await _ShuflContext.GroupMembers.Where(gm =>
+                gm.UserId == userId &&
+                gm.GroupId == groupId).FirstOrDefaultAsync();
+        }
     }
 }
--- Shufl.Domain.Repositories/Group/Interfaces/IGroupMemberRepository.cs
+++ Shufl.Domain.Repositories/Group/Interfaces/IGroupMemberRepository.cs
@@ -1,9 +1,12 @@
 ﻿using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Interfaces;
+using System;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group.Interfaces
 {
     public interface IGroupMemberRepository : IRepositoryBase<GroupMember>
     {
+        Task<GroupMember> GetByUserIdAndGroupIdAsync(Guid userId, Guid groupId);
     }
 }
