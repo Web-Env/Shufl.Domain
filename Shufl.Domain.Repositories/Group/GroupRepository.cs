21ff250638b9303aca48b28154b49ac3a50b26ef
AdamBOD <adambarryodonovan@gmail.com>
2021-03-27 00:14:27 +0000

    Added GetManyByIdForDownloadAsync method

--- Shufl.Domain.Repositories/Group/GroupRepository.cs
+++ Shufl.Domain.Repositories/Group/GroupRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Group.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +15,14 @@ namespace Shufl.Domain.Repositories.Group
         public async Task<Entities.Group> GetByIdentifierAsync(string groupIdentifier)
         {
             return await _ShuflContext.Groups.Where(u => u.Identifier == groupIdentifier).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Entities.Group>> GetManyByIdForDownloadAsync(IEnumerable<Guid> groupIds)
+        {
+            return await _ShuflContext.Groups
+                .Include(g => g.CreatedBy)
+                .Where(x => groupIds.Contains(EF.Property<Guid>(x, "Id")))
+                .ToListAsync();
         }
     }
 }
--- Shufl.Domain.Repositories/Group/Interfaces/IGroupRepository.cs
+++ Shufl.Domain.Repositories/Group/Interfaces/IGroupRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Shufl.Domain.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group.Interfaces
@@ -6,5 +8,7 @@ namespace Shufl.Domain.Repositories.Group.Interfaces
     public interface IGroupRepository : IRepositoryBase<Entities.Group>
     {
         Task<Entities.Group> GetByIdentifierAsync(string groupIdentifier);
+
+        Task<List<Entities.Group>> GetManyByIdForDownloadAsync(IEnumerable<Guid> ids);
     }
 }
