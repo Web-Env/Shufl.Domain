6a08c6764cf4203314e8170cff33bb5eddb1759b
AdamBOD <adambarryodonovan@gmail.com>
2021-03-27 00:20:08 +0000

    Updated GroupRepository with Download methods (#14)
    * Added GetManyByIdForDownloadAsync method
    
    * Added GetByIdForDownloadAsync

--- Shufl.Domain.Repositories/Group/Interfaces/IGroupRepository.cs
+++ Shufl.Domain.Repositories/Group/Interfaces/IGroupRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Shufl.Domain.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group.Interfaces
@@ -6,5 +8,9 @@ namespace Shufl.Domain.Repositories.Group.Interfaces
     public interface IGroupRepository : IRepositoryBase<Entities.Group>
     {
         Task<Entities.Group> GetByIdentifierAsync(string groupIdentifier);
+
+        Task<Entities.Group> GetByIdForDownloadAsync(Guid groupId);
+
+        Task<List<Entities.Group>> GetManyByIdForDownloadAsync(IEnumerable<Guid> ids);
     }
 }
