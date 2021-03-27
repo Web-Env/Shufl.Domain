0581305dcc000d015f2e3bb204ad7f20133bd7b2
AdamBOD <adambarryodonovan@gmail.com>
2021-03-27 01:07:42 +0000

    Added constructor for extended context

--- Shufl.Domain.Entities/ShuflDbContext.cs
+++ Shufl.Domain.Entities/ShuflDbContext.cs
@@ -4,6 +4,14 @@ namespace Shufl.Domain.Entities
 {
     public class ShuflDbContext : ShuflContext
     {
+        public ShuflDbContext()
+        {
+        }
+
+        public ShuflDbContext(DbContextOptions<ShuflContext> options)
+            : base(options)
+        {
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLazyLoadingProxies();
