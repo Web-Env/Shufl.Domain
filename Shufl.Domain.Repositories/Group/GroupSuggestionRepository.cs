40273a1e43ed013d5f652d73cfc5f2b8fa620809
AdamBOD <adambarryodonovan@gmail.com>
2021-03-29 21:05:55 +0100

    Added GetByIdentifierAndGroupIdAsync to GroupSuggestionRepository

--- Shufl.Domain.Repositories/Group/GroupSuggestionRepository.cs
+++ Shufl.Domain.Repositories/Group/GroupSuggestionRepository.cs
@@ -1,10 +1,21 @@
-﻿using Shufl.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Group.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group
 {
     public class GroupSuggestionRepository : RepositoryBase<GroupSuggestion>, IGroupSuggestionRepository
     {
         public GroupSuggestionRepository(ShuflContext context) : base(context) { }
+
+        public async Task<GroupSuggestion> GetByIdentifierAndGroupIdAsync(string groupSuggestionIdentifier, Guid groupId)
+        {
+            return await _ShuflContext.GroupSuggestions.Where(gs =>
+                gs.Identifier == groupSuggestionIdentifier &&
+                gs.GroupId == groupId).FirstOrDefaultAsync();
+        }
     }
 }
--- Shufl.Domain.Repositories/Group/Interfaces/IGroupSuggestionRepository.cs
+++ Shufl.Domain.Repositories/Group/Interfaces/IGroupSuggestionRepository.cs
@@ -1,9 +1,12 @@
 ﻿using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Interfaces;
+using System;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Group.Interfaces
 {
     public interface IGroupSuggestionRepository : IRepositoryBase<GroupSuggestion>
     {
+        Task<GroupSuggestion> GetByIdentifierAndGroupIdAsync(string groupSuggestionIdentifier, Guid groupId);
     }
 }
