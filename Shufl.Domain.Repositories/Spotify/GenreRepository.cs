d9cfffadcc8327bf1b763c66a7a192d705c96b66
AdamBOD <adambarryodonovan@gmail.com>
2021-03-29 22:15:28 +0100

    Added GetByCodeAsync to GenreRepository

--- Shufl.Domain.Repositories/Spotify/GenreRepository.cs
+++ Shufl.Domain.Repositories/Spotify/GenreRepository.cs
@@ -1,10 +1,18 @@
-﻿using Shufl.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Spotify.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Spotify
 {
     public class GenreRepository : RepositoryBase<Genre>, IGenreRepository
     {
         public GenreRepository(ShuflContext context) : base(context) { }
+
+        public async Task<Genre> GetByCodeAsync(string code)
+        {
+            return await _ShuflContext.Genres.Where(g => g.Code == code).FirstOrDefaultAsync();
+        }
     }
 }
--- Shufl.Domain.Repositories/Spotify/Interfaces/IGenreRepository.cs
+++ Shufl.Domain.Repositories/Spotify/Interfaces/IGenreRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Interfaces;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Spotify.Interfaces
 {
     public interface IGenreRepository : IRepositoryBase<Genre>
     {
+        Task<Genre> GetByCodeAsync(string code);
     }
 }
