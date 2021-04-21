06e7592454fabe8bcb8212d5c888b91b5b11221a
AdamBOD <adambarryodonovan@gmail.com>
2021-04-21 22:19:16 +0100

    Setup UserImage Repository

---
+++ Shufl.Domain.Repositories/User/Interfaces/IUserImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shufl.Domain.Entities;
+using Shufl.Domain.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shufl.Domain.Repositories.User.Interfaces
+{
+    public interface IUserImageRepository : IRepositoryBase<UserImage>
+    {
+        Task<IEnumerable<UserImage>> GetByUserIdAsync(Guid userId);
+    }
+}
---
+++ Shufl.Domain.Repositories/User/UserImageRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shufl.Domain.Entities;
+using Shufl.Domain.Repositories.User.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shufl.Domain.Repositories.User
+{
+    public class UserImageRepository : RepositoryBase<UserImage>, IUserImageRepository
+    {
+        public UserImageRepository(ShuflContext context) : base(context) { }
+
+        public async Task<IEnumerable<UserImage>> GetByUserIdAsync(Guid userId)
+        {
+            return await _ShuflContext.Set<UserImage>().Where(us => us.UserId == userId).ToListAsync();
+        }
+    }
+}
