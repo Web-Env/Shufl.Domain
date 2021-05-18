df91aff8479ea3ac9f60fbb010446a985911036f
AdamBOD <adambarryodonovan@gmail.com>
2021-05-18 18:44:32 +0100

    Updated Context

--- Shufl.Domain.Entities/PlaylistImage.cs
+++ Shufl.Domain.Entities/PlaylistImage.cs
@@ -11,7 +11,7 @@ namespace Shufl.Domain.Entities
         public Guid PlaylistId { get; set; }
         public short Width { get; set; }
         public short Height { get; set; }
-        public string Uri { get; set; }
+        public string Url { get; set; }
 
         public virtual Playlist Playlist { get; set; }
     }
