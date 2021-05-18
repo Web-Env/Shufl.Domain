9ed8f9bc2bf712f73768cca5047ce2114bcf142c
AdamBOD <adambarryodonovan@gmail.com>
2021-05-18 15:52:20 +0100

    Updated PlaylistRepository

--- Shufl.Domain.Repositories/Spotify/Interfaces/IPlaylistRepository.cs
+++ Shufl.Domain.Repositories/Spotify/Interfaces/IPlaylistRepository.cs
@@ -1,9 +1,8 @@
 ﻿using Shufl.Domain.Entities;
-using Shufl.Domain.Repositories.Interfaces;
 
 namespace Shufl.Domain.Repositories.Spotify.Interfaces
 {
-    public interface IPlaylistRepository : IRepositoryBase<Playlist>
+    public interface IPlaylistRepository : ISpotifyRepositoryBase<Playlist>
     {
     }
 }
--- Shufl.Domain.Repositories/Spotify/PlaylistRepository.cs
+++ Shufl.Domain.Repositories/Spotify/PlaylistRepository.cs
@@ -1,10 +1,25 @@
-﻿using Shufl.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Shufl.Domain.Entities;
 using Shufl.Domain.Repositories.Spotify.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Shufl.Domain.Repositories.Spotify
 {
     public class PlaylistRepository : RepositoryBase<Playlist>, IPlaylistRepository
     {
         public PlaylistRepository(ShuflContext context) : base(context) { }
+
+        public async Task<Playlist> CheckExistsBySpotifyIdAsync(string spotifyId)
+        {
+            return await _ShuflContext.Playlists.Where(p => p.SpotifyId == spotifyId).FirstOrDefaultAsync();
+        }
+
+        public async Task<Playlist> GetBySpotifyIdAsync(string spotifyId)
+        {
+            return await _ShuflContext.Playlists.Where(p => p.SpotifyId == spotifyId)
+                .Include(p => p.PlaylistImages)
+                .FirstOrDefaultAsync();
+        }
     }
 }
