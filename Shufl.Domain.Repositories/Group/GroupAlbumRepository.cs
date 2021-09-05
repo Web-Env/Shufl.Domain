adb7266aa4eec820cdfb58c8c2e31f30e3ecfa0f
AdamBOD <adambarryodonovan@gmail.com>
2021-09-05 20:13:56 +0100

    Updated GroupAlbumRepository (#37)

--- Shufl.Domain.Repositories/Group/GroupAlbumRepository.cs
+++ Shufl.Domain.Repositories/Group/GroupAlbumRepository.cs
@@ -16,21 +16,40 @@ namespace Shufl.Domain.Repositories.Group
         {
             var offset = pageSize * page;
 
-            return await _ShuflContext.GroupAlbums.Where(gs => gs.GroupId == groupId)
-                .OrderByDescending(gs => gs.CreatedOn)
+            return await _ShuflContext.GroupAlbums.Where(ga => ga.GroupId == groupId)
+                .OrderByDescending(ga => ga.CreatedOn)
                 .Skip(offset)
                 .Take(pageSize)
-                .Include(gs => gs.Album)
+                .Include(ga => ga.Album)
                     .ThenInclude(a => a.AlbumArtists)
                         .ThenInclude(aa => aa.Artist)
                             .ThenInclude(a => a.ArtistGenres)
                                 .ThenInclude(ag => ag.Genre)
-                .Include(gs => gs.Album)
+                .Include(ga => ga.Album)
                     .ThenInclude(a => a.AlbumImages)
-                .Include(gs => gs.GroupAlbumRatings)
+                .Include(ga => ga.GroupAlbumRatings)
                     .ThenInclude(gsr => gsr.CreatedByNavigation)
-                .Include(gs => gs.CreatedByNavigation)
+                .Include(ga => ga.CreatedByNavigation)
                     .ThenInclude(u => u.UserImages)
+                .AsSplitQuery()
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<int> GetCountByGroupIdAsync(Guid groupId)
+        {
+            return await _ShuflContext.GroupAlbums.Where(ga => ga.GroupId == groupId)
+                .CountAsync();
+        }
+
+        public async Task<IEnumerable<GroupAlbum>> GetTopThirtyByGroupIdAsync(Guid groupId)
+        {
+            return await _ShuflContext.GroupAlbums.Where(ga => ga.GroupId == groupId)
+                .OrderByDescending(ga => ga.GroupAlbumRatings.Average(gar => gar.OverallRating))
+                .Take(30)
+                .Include(ga => ga.Album)
+                    .ThenInclude(a => a.AlbumArtists)
+                        .ThenInclude(aa => aa.Artist)
                 .AsSplitQuery()
                 .AsNoTracking()
                 .ToListAsync();
@@ -38,17 +57,17 @@ namespace Shufl.Domain.Repositories.Group
 
         public async Task<GroupAlbum> GetByIdentifierAndGroupIdAsync(string groupAlbumIdentifier, Guid groupId)
         {
-            return await _ShuflContext.GroupAlbums.Where(gs => gs.Identifier == groupAlbumIdentifier && gs.Group.Id == groupId)
-                .Include(gs => gs.Album)
+            return await _ShuflContext.GroupAlbums.Where(ga => ga.Identifier == groupAlbumIdentifier && ga.Group.Id == groupId)
+                .Include(ga => ga.Album)
                     .ThenInclude(a => a.AlbumArtists)
                         .ThenInclude(aa => aa.Artist)
                             .ThenInclude(a => a.ArtistGenres)
                                 .ThenInclude(ag => ag.Genre)
-                .Include(gs => gs.Album)
+                .Include(ga => ga.Album)
                     .ThenInclude(a => a.AlbumImages)
-                .Include(gs => gs.GroupAlbumRatings)
+                .Include(ga => ga.GroupAlbumRatings)
                     .ThenInclude(gsr => gsr.CreatedByNavigation)
-                .Include(gs => gs.CreatedByNavigation)
+                .Include(ga => ga.CreatedByNavigation)
                 .AsSplitQuery()
                 .AsNoTracking()
                 .FirstOrDefaultAsync();
@@ -56,9 +75,9 @@ namespace Shufl.Domain.Repositories.Group
         
         public async Task<GroupAlbum> CheckExistsByIdentifierAndGroupIdAsync(string groupAlbumIdentifier, Guid groupId)
         {
-            return await _ShuflContext.GroupAlbums.Where(gs =>
-                gs.Identifier == groupAlbumIdentifier &&
-                gs.GroupId == groupId).FirstOrDefaultAsync();
+            return await _ShuflContext.GroupAlbums.Where(ga =>
+                ga.Identifier == groupAlbumIdentifier &&
+                ga.GroupId == groupId).FirstOrDefaultAsync();
         }
     }
 }
